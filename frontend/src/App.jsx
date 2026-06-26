3da24a816fcd3ccf9bfe48bc4643e9a977b03d5a
AMXAI <amxaioffical@gmail.com>
2026-06-26 13:54:21 +0530

    Adding about section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./layouts/navbar.jsx";
 
 import Footer from "./layouts/footer.jsx";
+import About from "./components/home/aboutSection.jsx";
 
 function App() {
     return (
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" />
-                <Route path="/about" />
-                <Route path="/service" />
-                <Route path="/contact" />
+                <Route path="/" element={<div>Home</div>} />
+                <Route path="/about" element={<About />} />
+                <Route path="/service" element={<div>Service</div>} />
+                <Route path="/contact" element={<div>Contact</div>} />
             </Routes>
-
             <Footer />
         </Router>
     );
